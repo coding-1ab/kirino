5faf5e055889fe24c97ae9efa7c9468092657d2e
Abiria <abiria.dev@gmail.com>
2024-09-03 11:20:49 +0900

    fix: use inner client object

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,14 +1,14 @@
 import { ApplyOptions } from '@sapphire/decorators'
 import { Listener } from '@sapphire/framework'
-import type { Client } from 'discord.js'
 
 @ApplyOptions<Listener.Options>({})
 export class ReadyEvent extends Listener {
-	public override run(client: Client) {
-		const { username } = client.user!
+	public override run() {
+		const { username, discriminator } =
+			this.container.client.user!
 
 		this.container.logger.info(
-			`Logged in as ${username}`,
+			`Logged in as ${username}#${discriminator}`,
 		)
 	}
 }
